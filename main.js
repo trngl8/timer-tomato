e2b5b36e09914cfb45bacedc9dbc09b95d14d1dc
Red Rubber Duck <mvch@triangle.software>
2023-11-26 22:03:11 +0200

    notification window, close #3

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ let timer;
 let time = 0;
 let isTimerRunning = false;
 
+//default, granted, denied
+console.log(Notification.permission)
+
+function showNotification() {
+    const notification = new Notification("Alarm", {
+        body: 'Time is out',
+        icon: "images/alarm.svg"
+    })
+
+    notification.onclick = (e) => {
+        window.location.href = "#";
+    }
+}
+
+function checkPermission() {
+    if(Notification.permission === "granted") {
+        showNotification();
+    } else if(Notification.permission !== "denied") {
+        Notification.requestPermission().then(permission => {
+            if(permission === "granted") {
+                showNotification()
+            }
+        })
+    }
+}
+
 function startTimer() {
     if (!isTimerRunning) {
         isTimerRunning = true;
@@ -30,5 +56,9 @@ function updateTimerDisplay() {
     const minutes = Math.floor(time / (60 * 100));
     const seconds = Math.floor((time % (60 * 100)) / 100);
     const milliseconds = time % 100;
+    if (minutes === 25) {
+        checkPermission();
+        resetTimer();
+    }
     document.getElementById('timer-output').innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(2, '0')}`;
 }
